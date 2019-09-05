b11cee68b11d0fc202a18812589ce0dce35bdc8f
Bruno Damasco <brunodamasco1@gmail.com>
2019-09-05 13:20:31 -0300

    Initial commit

---
+++ app/src/main/java/com/example/exemplolistview/control/MainControl.java
@@ -0,0 +1,159 @@
+package com.example.exemplolistview.control;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.exemplolistview.R;
+import com.example.exemplolistview.model.Estado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainControl {
+    private Activity activity;
+    private EditText editNome;
+    private EditText editSigla;
+    private ListView lvEstado;
+    private List<Estado> listEstado;
+    private ArrayAdapter<Estado> adapterEstado;
+    private Estado estado;
+    private TextView tvContador;
+
+    public MainControl(Activity activity) {
+        this.activity = activity;
+        initComponents();
+    }
+
+    public void initComponents(){
+        editNome = activity.findViewById(R.id.editNome);
+        editSigla = activity.findViewById(R.id.editSigla);
+        lvEstado = activity.findViewById(R.id.lvEstado);
+        tvContador = activity.findViewById(R.id.tvContador);
+        configListView();
+    }
+
+    public void configListView(){
+        listEstado = new ArrayList<>();
+        //listEstado.add(new Estado("Santa Catarina", "SC"));
+        adapterEstado = new ArrayAdapter<>(
+                activity,
+                android.R.layout.simple_list_item_1,
+                listEstado
+        );
+        lvEstado.setAdapter(adapterEstado);
+        cliqueCurto();
+        cliqueLongo();
+    }
+
+    public void cliqueCurto(){
+        lvEstado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                estado = adapterEstado.getItem(position);
+                confirmarEdicao(estado);
+            }
+        });
+    }
+    public void cliqueLongo(){
+        lvEstado.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                estado = adapterEstado.getItem(i);
+                confirmarExclusao(estado);
+                //return false; // Executa o clique curto junto
+                return true; // Não executa o clique curto junto
+
+            }
+        });
+
+    }
+
+    private Estado getDadosForm(){
+        Estado e = new Estado();
+        e.setNome(editNome.getText().toString());
+        e.setSigla(editSigla.getText().toString());
+        return e;
+    }
+
+    private void addEstadoLv(Estado e){
+        adapterEstado.add(e);
+    }
+
+    private void alterarEstado(Estado e){
+        estado.setNome(e.getNome());
+        estado.setSigla(e.getSigla());
+        adapterEstado.notifyDataSetChanged();
+    }
+
+    private void excluirEstadoLv(Estado e){
+        adapterEstado.remove(e);
+        atualizarContador();
+    }
+
+    private void confirmarExclusao(final Estado e){
+        AlertDialog.Builder alerta = new AlertDialog.Builder(activity);
+        alerta.setTitle("Excluir Estado");
+        alerta.setMessage(e.toString());
+        alerta.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                estado = null;
+            }
+        });
+        alerta.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                excluirEstadoLv(e);
+                estado = null;
+            }
+        });
+        alerta.show();
+    }
+
+    private void carregarForm(Estado e){
+        editNome.setText(e.getNome());
+        editSigla.setText(e.getSigla());
+    }
+
+    private void atualizarContador(){
+        tvContador.setText("Contador: " + adapterEstado.getCount());
+    }
+
+    public void salvarAction(){
+        if (estado==null){
+            Estado e = getDadosForm();
+            addEstadoLv(e);
+            atualizarContador();
+        } else {
+            Estado e = getDadosForm();
+            alterarEstado(e);
+        }
+        estado = null;
+    }
+    private void confirmarEdicao(final Estado e){
+        AlertDialog.Builder alerta = new AlertDialog.Builder(activity);
+        alerta.setTitle("Mostrar dados");
+        alerta.setMessage(e.toString());
+        alerta.setNegativeButton("Fechar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                estado = null;
+            }
+        });
+        alerta.setPositiveButton("Editar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                carregarForm(e);
+            }
+        });
+        alerta.show();
+    }
+}
+
---
+++ app/src/main/java/com/example/exemplolistview/model/Estado.java
@@ -0,0 +1,32 @@
+package com.example.exemplolistview.model;
+
+import java.io.Serializable;
+
+public class Estado implements Serializable {
+    private String nome;
+    private String sigla;
+
+    public Estado() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " ("+sigla+")";
+    }
+}
